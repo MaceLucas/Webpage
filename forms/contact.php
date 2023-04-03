471bb598efa27b611098de108e85887dda72f90e
MaceLucas <gaurabpandey03@gmail.com>
2023-04-03 18:41:44 +0545

    ok

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,11 @@
 <?php
   $receiving_email_address = 'gaurabpandey03@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  $messages_file = 'message.txt';
+  
+  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" library!');
   }
 
   $contact = new PHP_Email_Form;
@@ -15,10 +16,19 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
   
+  $contact->add_message($_POST['name'], 'From');
+  $contact->add_message($_POST['email'], 'Email');
+  $contact->add_message($_POST['message'], 'Message', 10);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $message = $contact->get_message();
+
+  if (file_put_contents($messages_file, $message . PHP_EOL, FILE_APPEND | LOCK_EX) === false) {
+    die('Failed to write message to file!');
+  }
+
+  if (chmod($messages_file, 0600) === false) {
+    die('Failed to set permissions on messages file!');
+  }
 
   echo $contact->send();
 ?>
